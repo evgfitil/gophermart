02f607f85c23697c595a56f544d4b763eddae751
Evgenii Akhmetzianov <erakhmetzyan@yandex-team.ru>
2024-05-04 11:44:27 +0300

    add custom errors

---
+++ internal/apperrors/errors.go
@@ -0,0 +1,8 @@
+package apperrors
+
+import "errors"
+
+var (
+	ErrOrderAlreadyExists = errors.New("order already exists")
+	ErrOrderNumberTaken   = errors.New("order number already taken by another user")
+)
