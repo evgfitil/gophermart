e182db30e938ce880f82ac8cd77ad3fcb93ab469
Evgenii Akhmetzianov <erakhmetzyan@yandex-team.ru>
2024-05-05 20:22:02 +0300

    Change JSON OrderNumber annotation

--- internal/models/order.go
+++ internal/models/order.go
@@ -7,7 +7,7 @@ import (
 type Order struct {
 	ID          int       `json:"-"`
 	UserID      int       `json:"-"`
-	OrderNumber string    `json:"order_number"`
+	OrderNumber string    `json:"number"`
 	Status      string    `json:"status"`
 	Accrual     float64   `json:"accrual,omitempty"`
 	UploadedAt  time.Time `json:"uploaded_at"`
